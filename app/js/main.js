307ddd123682ada4c022215521eee8d45b77c4f3
lvan226 <lvan226@list.ru>
2019-09-28 19:11:30 +0800

    Added news-item page

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
 $(function(){
 
   
-  var mixer = mixitup('.products__inner-box');
+
 
   $('.rate-star').rateYo({
     rating: 5,
@@ -15,5 +15,29 @@ $(function(){
     slidesToShow: 4,
     slidesToScroll: 4,
   });
+
+  $(".js-range-slider").ionRangeSlider({
+    type: 'double',
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    grid: false,
+    prefix: "$"
+  });
+
+  $(".icon-th-list").on('click', function() {
+    $(".product-page__content .product__item").addClass('list');
+    $(".icon-th-list").addClass('active');
+    $(".icon-th-large").removeClass('active');
+  });
+
+  $(".icon-th-large").on('click', function() {
+    $(".product-page__content .product__item").removeClass('list');
+    $(".icon-th-large").addClass('active');
+    $(".icon-th-list").removeClass('active');
+  });
+
+  var mixer = mixitup('.products__inner-box');
 
 });
